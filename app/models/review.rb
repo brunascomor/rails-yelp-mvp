bb3b3b59fe4c773dbc63537993aa6da2c76688c0
Bruna Scopel <brunascomor@gmail.com>
2020-11-26 08:54:05 +0100

    models and controllers created

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: 0..5, numericality: { only_integer: true }
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+categories = %w[chinese italian japanese french belgian]
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: categories.sample,
+    phone_number: "12345678"
+  )
+end
